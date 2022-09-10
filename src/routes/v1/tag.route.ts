f78487dbef6e57ec2ed469105ab5a3ad3a249ecb
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-09-10 23:41:30 +0500

    added swagger docs for tag endpoints

--- src/routes/v1/tag.route.ts
+++ src/routes/v1/tag.route.ts
@@ -17,3 +17,174 @@ router
   .delete(validate(tagValidation.deleteTag), tagController.deleteTagById);
 
 export default router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Tag
+ *   description: Tags management and retrieval
+ */
+
+/**
+ * @swagger
+ * /tags:
+ *   post:
+ *     summary: Create a tag
+ *     description: Creates a new tag.
+ *     tags: [Tag]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - tag_name
+ *             properties:
+ *               tag_name:
+ *                 type: string
+ *               tag_description:
+ *                 type: string
+ *                 description: can be null
+ *             example:
+ *                tag_name: enhancement
+ *                tag_description: improvement tag
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Tag'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ *
+ *   get:
+ *     summary: Get all tags
+ *     description: Returns a list of tags.
+ *     tags: [Tag]
+ *     parameters:
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *         default: 10
+ *         description: Maximum number of tags
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           minimum: 1
+ *           default: 1
+ *         description: Page number
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Tag'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 total_pages:
+ *                   type: integer
+ *                   example: 1
+ *                 total_results:
+ *                   type: integer
+ *                   example: 1
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+
+/**
+ * @swagger
+ * /tags/{tag_id}:
+ *   get:
+ *     summary: Get a tag by id
+ *     description: Get details about a tag by their id
+ *     tags: [Tag]
+ *     parameters:
+ *       - in: path
+ *         name: tag_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Tag id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Tag'
+ *       "404":
+ *         $ref: '#/components/responses/TagNotFound'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ *
+ *   patch:
+ *     summary: Update a tag by id
+ *     description: Update tag details by their id
+ *     tags: [Tag]
+ *     parameters:
+ *       - in: path
+ *         name: tag_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Tag id
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               tag_name:
+ *                 type: string
+ *               tag_description:
+ *                 type: string
+ *                 description: can be null
+ *             example:
+ *                tag_name: enhancement
+ *                tag_description: improvement tag
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Tag'
+ *       "404":
+ *         $ref: '#/components/responses/TagNotFound'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ *
+ *   delete:
+ *     summary: Delete a tag by id
+ *     description: Deletes a tag by their id
+ *     tags: [Tag]
+ *     parameters:
+ *       - in: path
+ *         name: tag_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Tag id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *       "404":
+ *         $ref: '#/components/responses/TagNotFound'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ */
