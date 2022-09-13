f4d6a0aa3ca95179b1e3d36c77ca8c572e30a3dc
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-09-14 00:30:28 +0500

    updated header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,82 +1,119 @@
-import ClearIcon from '@mui/icons-material/Clear';
-import MenuIcon from '@mui/icons-material/Menu';
+// import './responsive-menu-transition.css';
+// import ClearIcon from '@mui/icons-material/Clear';
+// import MenuIcon from '@mui/icons-material/Menu';
+import {
+  // Button,
+  Grid,
+  // TextField
+} from '@mui/material';
 import React from 'react';
-
-import icon1 from "../../assets/icon-1.png";
-import icon2 from "../../assets/icon-2.png";
-import logo from "../../assets/logo.png";
-
-import { Button, Grid, TextField } from '@mui/material';
 import { Link, useLocation } from 'react-router-dom';
+// import { CSSTransition } from 'react-transition-group';
+// import MenuIcon4 from "../../assets/bank-icon.png";
+// import MenuIcon4Active from "../../assets/bank-icon-2.png";
+// import MenuIcon2 from "../../assets/book-icon.png";
+// import MenuIcon2Active from "../../assets/book-icon-2.png";
+// import MenuIcon6 from "../../assets/credit.png";
+// import MenuIcon6Active from "../../assets/credit-icon-2.png";
+// import MenuIcon3 from "../../assets/envelope-icon.png";
+// import MenuIcon3Active from "../../assets/envelope-icon-2.png";
+// import icon1 from "../../assets/icon-1.png";
+// import icon2 from "../../assets/icon-2.png";
+import logo from "../../../assets/logo-1.png";
+// import MenuIcon5 from "../../assets/wallet-icon.png";
+// import MenuIcon5Active from "../../assets/wallet-icon-2.png";
+import "./Header.css";
 
-import MenuIcon4 from "../../assets/bank-icon.png";
-import MenuIcon2 from "../../assets/book-icon.png";
-import MenuIcon6 from "../../assets/credit.png";
-import MenuIcon3 from "../../assets/envelope-icon.png";
-import MenuIcon5 from "../../assets/wallet-icon.png";
+// const duration = 200000;
 
-import { CSSTransition } from 'react-transition-group';
-import MenuIcon4Active from "../../assets/bank-icon-2.png";
-import MenuIcon2Active from "../../assets/book-icon-2.png";
-import MenuIcon6Active from "../../assets/credit-icon-2.png";
-import MenuIcon3Active from "../../assets/envelope-icon-2.png";
-import MenuIcon5Active from "../../assets/wallet-icon-2.png";
+// const Fade = (props) => {
+//   return (
+//     <CSSTransition classNames="alert" in={props.inProp} timeout={duration} unmountOnExit>
+//       {props.children}
+//     </CSSTransition>
+//   );
+// };
 
-import './responsive-menu-transition.css';
-
-const duration = 200000;
-
-const Fade = (props) => {
-  return (
-    <CSSTransition classNames="alert" in={props.inProp} timeout={duration} unmountOnExit>
-      {props.children}
-    </CSSTransition>
-  );
-};
-
-function Header() {
+function HomeHeader({ displayNav = false }) {
   const location = useLocation();
 
-  const [displayResponsiveMenu, setDisplayResponsiveMenu] = React.useState(false);
+  // const [displayResponsiveMenu, setDisplayResponsiveMenu] = React.useState(false);
 
   return (
-    <Grid container className='header'>
-      <Grid item md={2} className='logoContainer responsive'>
-        <Link to="/"><img alt='logo' src={logo} /></Link>
+    <Grid container className="header" alignItems='center'>
+      <Grid
+        item
+        md={3}
+        display='flex'
+        justifyContent='flex-start'
+        alignItems='center'
+      >
+        <Link to="/"><img alt='logo' src={logo} className='site-logo' /></Link>
       </Grid>
 
-      <Grid item md={8} className='responsive'>
+      {/* <Grid item md={4} className='responsive'>
         <TextField variant="standard"
           InputProps={{
             disableUnderline: true,
             style: { padding: '6px 17px', color: '#32363C' },
           }}
           className='searchBar' fullWidth placeholder="Search for online course" id="fullWidth" />
+      </Grid> */}
+
+      <Grid item md={6} alignItems='center'>
+        {displayNav && <nav className='site-nav'>
+        <ul>
+            <li className={location.pathname === '/creations' ? 'activeSidebarMenu' : ''}><Link to='/creations'>Creations</Link></li>
+            <li className={location.pathname === '/invitation' ? 'activeSidebarMenu' : ''} ><Link to='/invitation'>Invitation</Link></li>
+            <li className={location.pathname === '/litigation' ? 'activeSidebarMenu' : ''} ><Link to='/litigation'>Litigation</Link></li>
+            <li className={location.pathname === '/wallet' ? 'activeSidebarMenu' : ''} ><Link to='/wallet'>Wallet</Link></li>
+            <li className={location.pathname === '/credit' ? 'activeSidebarMenu' : ''} ><Link to='/credit'>Credit</Link></li>
+          </ul>
+        </nav>}
+        {/* {displayNav && <nav
+         className="site-nav"
+        >
+          <ul>
+            <li className={location.pathname === '/creations' ? 'activeSidebarMenu' : ''}><Link to='/creations'>Creations</Link></li>
+            <li className={location.pathname === '/invitation' ? 'activeSidebarMenu' : ''} ><Link to='/invitation'>Invitation</Link></li>
+            <li className={location.pathname === '/litigation' ? 'activeSidebarMenu' : ''} ><Link to='/litigation'>Litigation</Link></li>
+            <li className={location.pathname === '/wallet' ? 'activeSidebarMenu' : ''} ><Link to='/wallet'>Wallet</Link></li>
+            <li className={location.pathname === '/credit' ? 'activeSidebarMenu' : ''} ><Link to='/credit'>Credit</Link></li>
+          </ul>
+        </nav>} */}
       </Grid>
 
-      <Grid item xs={6} className='non-responsive responsiveMenuBar'>
-        <Button onClick={() => setDisplayResponsiveMenu(true)}><MenuIcon /></Button>
-      </Grid>
+      {/* <Grid item xs={6} className='non-responsive responsiveMenuBar'>
+        <Button onClick={(e) => setDisplayResponsiveMenu(true)}><MenuIcon /></Button>
+      </Grid> */}
 
-      <Grid item md={2} xs={6} className='headerMenu'>
-        <ul style={{ display: 'flex', justifyContent: 'flex-end', gap: '18px', alignItems: 'center', marginTop: '45px' }}>
-          <li className='menuBox'><img alt='icon-menu-1' src={icon1} /></li>
-          <li className='menuBox'><img alt='icon-menu-1' src={icon2} /></li>
-          <li style={{ display: 'flex', alignItems: 'center', gap: '5px' }}><img alt='user-img' style={{width: '40px', border: '1px solid #fff', borderRadius: '7px' }} src={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/156905/profile/profile-512.jpg?1530296477'} /><span className='responsive'>John Doe</span></li>
+      <Grid
+        item
+        md={3}
+        display='flex'
+        justifyContent='flex-end'
+        alignItems='center'
+      >
+        <ul style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+          {/* <li className='menuBox'><img alt='icon-menu-1' src={icon1} /></li>
+          <li className='menuBox'><img alt='icon-menu-1' src={icon2} /></li> */}
+          <li style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+            <img alt='user-img' style={{ width: '40px', border: '1px solid #fff', borderRadius: '7px' }} src={'https://s3-us-west-2.amazonaws.com/s.cdpn.io/156905/profile/profile-512.jpg?1530296477'} />
+            <span className='responsive'>John Doe</span>
+          </li>
         </ul>
       </Grid>
 
-      <Grid item xs={12} className='non-responsive'>
+      {/* <Grid item xs={12} className='non-responsive'>
         <TextField variant="standard"
           InputProps={{
             disableUnderline: true,
             style: { padding: '6px 17px', color: '#32363C' },
           }}
           className='searchBar' fullWidth placeholder="Search for online course" id="fullWidth" />
-      </Grid>
+      </Grid> */}
 
-
-      <Fade inProp={displayResponsiveMenu}>
+      {/* <Fade inProp={displayResponsiveMenu}>
         <header>
           <nav>
             <Grid container>
@@ -118,10 +155,9 @@ function Header() {
           </ul>
           </nav>
         </header>
-      </Fade>
-
+      </Fade> */}
     </Grid>
   );
 }
 
-export default Header;
+export default HomeHeader;
