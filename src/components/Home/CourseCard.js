cf77cc30dd9292ad7d2065d66113bf6c20ece276
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-20 14:13:17 -0230

    Update CourseCard.js

--- src/components/Home/CourseCard.js
+++ src/components/Home/CourseCard.js
@@ -1,21 +1,20 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from '@mui/material';
 import CourseCard1 from '../../assets/course-card1.png';
 
-function CourseCard(props) {
-
+function CourseCard() {
   return (
     <Grid container className="invitationCard courseCard">
       <Grid item xs={12}>
         <div className="invitationCardLeft courseCardLeft">
           <div className="invitationCardLeftImage courseCardLeftImage">
-            <img alt='course-card' width='180' src={CourseCard1} />
+            <img alt="course-card" width="180" src={CourseCard1} />
           </div>
           <div className="invitationCardLeftText courseCardLeftText">
-            <Typography variant='h6'>
-                Flutter building
+            <Typography variant="h6">
+              Flutter building
             </Typography>
-            <Typography variant='span'>
-                1000+ free files you deliver real-time metrics for actionalble results.
+            <Typography variant="span">
+              1000+ free files you deliver real-time metrics for actionalble results.
             </Typography>
 
             <Button>Enroll Course</Button>
