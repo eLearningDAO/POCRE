f1a3da6b56e2285b5fec4cbabf6a757817dcb4c3
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-20 14:04:54 -0230

    Update index.js

--- src/components/Creations/index.js
+++ src/components/Creations/index.js
@@ -1,16 +1,18 @@
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import {
+  Button, Grid, TextField, Typography,
+} from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 // import LearnHowToImage from '../../assets/learnhowto.png';
 import CollectionCard from './CollectionCard/CollectionCard';
-import "./index.css"
+import './index.css';
 
-const Creations = () => {
+function Creations() {
   const navigate = useNavigate();
 
   const add = () => {
     navigate('/creations/create');
-  }
+  };
 
   return (
     <Grid container spacing={2}>
@@ -24,51 +26,37 @@ const Creations = () => {
         height="fit-content"
         padding={{ xs: '12px', md: '0' }}
       >
-        <Typography className='heading h4' variant='h4'>My Creations</Typography>
-        <Grid gap={{ sm: "8px" }} display="flex" height="fit-content" flexDirection="row">
-          <TextField variant="standard"
+        <Typography className="heading h4" variant="h4">My Creations</Typography>
+        <Grid gap={{ sm: '8px' }} display="flex" height="fit-content" flexDirection="row">
+          <TextField
+            variant="standard"
             InputProps={{
               disableUnderline: true,
-              padding: 0
+              padding: 0,
             }}
             fullWidth
-            className='input search-bar' placeholder="Search" id="fullWidth" />
-          <Button onClick={add} className='btn btn-primary'>+ Add New</Button>
+            className="input search-bar"
+            placeholder="Search"
+            id="fullWidth"
+          />
+          <Button onClick={add} className="btn btn-primary">+ Add New</Button>
         </Grid>
       </Grid>
 
-      <Grid xs={12}
+      <Grid
+        xs={12}
         item
         display="flex"
         flexDirection={{ xs: 'row', md: 'column' }}
-        overflow={{ xs: 'scroll', md: "initial" }}
+        overflow={{ xs: 'scroll', md: 'initial' }}
         bgcolor="transparent"
         gap={{ xs: '16px' }}
         className="hidden-scrollbar"
         padding={{ xs: '12px', md: '0' }}
       >
-        {[1, 2, 3, 4, 5].map(() => <CollectionCard interactionBtns={true} />)}
+        {[1, 2, 3, 4, 5].map(() => <CollectionCard interactionBtns />)}
       </Grid>
 
-      {/* <Grid item xs={12} sm={12} md={9}>
-        <Typography className='inviationHeaderTitle invitationSentSection' variant='h4'>Trending Creations</Typography>
-
-        {[1, 2, 3].map(i => {
-          return (
-            <Grid item xs={12} style={{ paddingTop: '10px' }}>
-              <CollectionCard interactionBtns={false} />
-            </Grid>
-          );
-        })}
-      </Grid>
-
-      <Grid item md={3} xs={12} sm={12}>
-        <Typography className='inviationHeaderTitle invitationSentSection' variant='h4'>Learn How To</Typography>
-
-        <div className='learnHowToImage'>
-          <img src={LearnHowToImage} alt=""/>
-        </div>
-      </Grid> */}
     </Grid>
   );
 }
