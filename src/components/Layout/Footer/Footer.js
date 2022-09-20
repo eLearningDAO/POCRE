1f79f4ba87b2d8a02e9d398176e2aa8b8732ce53
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-20 13:59:59 -0230

    Update Footer.js

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,6 +1,8 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography } from '@mui/material';
 import TwitterIcon from '../../../assets/twitter2.png';
-import "./Footer.css"
+import Globe from '../../../assets/globe.png';
+import GitHub from '../../../assets/github.png';
+import './Footer.css';
 
 function Footer() {
   return (
@@ -8,9 +10,15 @@ function Footer() {
       <Grid item md={6} xs={9}>
         <Typography color="#32363C">Copyright © 2022 e-Learning DAO. Use and distribution are covered by AGPL-3.0</Typography>
       </Grid>
-      <Grid item md={6} xs={3} display="flex" justifyContent="flex-end" alignItems="center">
+      <Grid item md={6} xs={3} display="flex" justifyContent="flex-end" gap="3px" alignItems="center">
         <a href="https://twitter.com/eLearningDAO">
           <img src={TwitterIcon} alt="@eLearningDAO" />
+        </a>
+        <a href="https://www.pocre.net/">
+          <img src={Globe} alt="@eLearningDAO" width={30} height={30} />
+        </a>
+        <a href="https://github.com/e-Learning-DAO/POCRE\">
+          <img src={GitHub} alt="github logo" width={30} height={30} />
         </a>
       </Grid>
     </Grid>
