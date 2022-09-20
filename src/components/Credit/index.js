7ace2ef4d1d9d05d22e5a9f61611273f05d7bca4
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-20 14:05:21 -0230

    Update index.js

--- src/components/Credit/index.js
+++ src/components/Credit/index.js
@@ -3,37 +3,35 @@ import React from 'react';
 import CreditCard from './CreditCard';
 import OtherContributerCard from './OtherContributerCard';
 
-const Credit = () => {
-  return ( 
+function Credit() {
+  return (
     <Grid container spacing={2}>
 
-        <Grid item md={9} xs={12} className='topContributor'>
-            <Grid item md={12}>
-                <Typography className='inviationHeaderTitle' variant='h6'>Top Contributors</Typography>
-            </Grid>
-
-            <Grid item md={12} className='topContributorSection'>
-                <CreditCard name='John' role='e-learner Developer' bio='A junior web developer' />
-                <CreditCard name='Jacob' role='e-learner Developer' bio='A junior web developer' />
-                <CreditCard name='Peter' role='e-learner Developer' bio='A junior researcher developer' />
-                <CreditCard name='Eddy' role='e-learner Researcher' bio='A senior researcher' />
-            </Grid>
+      <Grid item md={9} xs={12} className="topContributor">
+        <Grid item md={12}>
+          <Typography className="inviationHeaderTitle" variant="h6">Top Contributors</Typography>
         </Grid>
 
-        <Grid item md={3} xs={12}>
-            <Grid item md={12}>
-                <Typography className='inviationHeaderTitle' variant='h6'>Others Contributors</Typography>
-            </Grid>
-
-            <Grid item md={12}  className='otherContributorSection'>
-            <OtherContributerCard name='John Wick' roles='Programmer, Architect, Developer'/>
-            <OtherContributerCard name='James Bond' roles='Writer, Architect, Developer'/>
-            <OtherContributerCard name='Adi Nate' roles='UI Designer, Architect, Developer'/>
-            <OtherContributerCard name='Peter Parker' roles='Photographer, Architect, Developer'/>
-            </Grid>
+        <Grid item md={12} className="topContributorSection">
+          {[1, 2, 3, 4, 5, 6].map(() => (
+            <CreditCard />
+          ))}
         </Grid>
+      </Grid>
+
+      <Grid item md={3} xs={12}>
+        <Grid item md={12}>
+          <Typography className="inviationHeaderTitle" variant="h6">Others Contributors</Typography>
+        </Grid>
+
+        <Grid item md={12} className="otherContributorSection">
+          {[1, 2, 3, 4].map(() => (
+            <OtherContributerCard />
+          ))}
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
- 
+
 export default Credit;
