f1407e490fb77561d7e493aec43e8e9ae0a65378
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-09-21 19:23:34 +0500

    added litigations home page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import {
 import Creations from './components/Creations';
 import CreateCreation from './components/Creations/Create';
 import Invitation from './components/Invitation';
-import Litigation from './components/Litigation';
+import LitigationDetails from './components/Litigation';
+import LitigationHome from './components/Litigation/LitigationHome';
 import LitigationClosed from './components/Litigation/LitigationClosed';
 import Wallet from './components/Wallet';
 import Layout from './components/Layout';
@@ -21,7 +22,8 @@ function App() {
         <Route path="/creations" element={<Layout displaySidebar><Creations /></Layout>} />
         <Route path="/creations/create" element={<Layout displaySidebar><CreateCreation /></Layout>} />
         <Route path="/invitation" element={<Layout displaySidebar><Invitation /></Layout>} />
-        <Route path="/litigation" element={<Layout displaySidebar><Litigation /></Layout>} />
+        <Route path="/litigation" element={<Layout displaySidebar><LitigationHome /></Layout>} />
+        <Route path="/litigation/details" element={<Layout displaySidebar><LitigationDetails /></Layout>} />
         <Route path="/litigation/closed" element={<Layout displaySidebar><LitigationClosed /></Layout>} />
         <Route path="/wallet" element={<Layout displaySidebar><Wallet /></Layout>} />
         <Route path="/credit" element={<Layout displaySidebar><Credit /></Layout>} />
