cccf5722dc1e1ff8f6c678bce0e758dd5d94edc6
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-09-21 22:29:57 +0500

    added litigation preview card on litigation-closed screen

--- src/components/Litigation/LitigationClosed.js
+++ src/components/Litigation/LitigationClosed.js
@@ -1,6 +1,10 @@
 import { Button, Grid, Typography } from '@mui/material';
 import React from 'react';
 import UserCard from '../cards/UserCard';
+import LitigationCard from '../cards/LitigationCard';
+import LikeIcon from '../../assets/svgs/like.svg';
+import DislikeIcon from '../../assets/svgs/dislike.svg';
+import ThumbPinIcon from '../../assets/svgs/thumb-pin.svg';
 
 export default function LitigationClosed() {
   return (
@@ -18,6 +22,24 @@ export default function LitigationClosed() {
           <Typography className="inviationSectionTitle" variant="h6">Claimer</Typography>
           <UserCard variant="secondary" />
         </Grid>
+      </Grid>
+
+      <Grid display="flex" marginTop="36px" width="100%">
+        <LitigationCard />
+      </Grid>
+      <Grid display="flex" gap="12px" marginTop="24px" alignItems="center" justifyContent="flex-end">
+        <Button className="btn bg-green color-white">
+          <img src={LikeIcon} alt="" style={{ marginRight: '8px' }} />
+          Agree
+        </Button>
+        <Button className="btn bg-red color-white">
+          <img src={DislikeIcon} alt="" style={{ marginRight: '8px' }} />
+          Opposition
+        </Button>
+        <Button className="btn bg-purple color-white">
+          <img src={ThumbPinIcon} alt="" style={{ marginRight: '8px' }} />
+          Impartial
+        </Button>
       </Grid>
 
       <Grid item xs={12} style={{ marginTop: '40px' }}>
