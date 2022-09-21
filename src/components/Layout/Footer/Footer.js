f78bac3965d9a85f5e94c598746040204a755751
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-21 09:40:34 -0230

    Cleaned UI--solved 1.Wrong tab implementation 2. Removal of followers 3. Button name changed to switch wallet 4. Given border-radius to Litigation Opening 5.Footer updated

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,5 +1,7 @@
 import { Grid, Typography } from '@mui/material';
 import TwitterIcon from '../../../assets/twitter2.png';
+import Github from '../../../assets/github.svg';
+import Globe from '../../../assets/globe.png';
 import './Footer.css';
 
 function Footer() {
@@ -8,9 +10,15 @@ function Footer() {
       <Grid item md={6} xs={9}>
         <Typography color="#32363C">Copyright © 2022 e-Learning DAO. Use and distribution are covered by AGPL-3.0</Typography>
       </Grid>
-      <Grid item md={6} xs={3} display="flex" justifyContent="flex-end" alignItems="center">
+      <Grid item gap={2} md={6} xs={3} display="flex" justifyContent="flex-end" alignItems="center">
         <a href="https://twitter.com/eLearningDAO">
           <img src={TwitterIcon} alt="@eLearningDAO" />
+        </a>
+        <a href="https://github.com/e-Learning-DAO/POCRE">
+          <img src={Github} alt="@eLearningDAO" width={30} height={30} />
+        </a>
+        <a href="https://pocre.net/">
+          <img src={Globe} alt="@eLearningDAO" width={30} height={30} />
         </a>
       </Grid>
     </Grid>
