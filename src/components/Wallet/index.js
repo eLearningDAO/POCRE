64f7c15f082f2555d206381fba3ca7a0f668b91b
Blazecode200 <69413640+adisagar2003@users.noreply.github.com>
2022-09-29 23:04:17 -0230

    Wallet section updated

--- src/components/Wallet/index.js
+++ src/components/Wallet/index.js
@@ -50,7 +50,7 @@ function Wallet() {
                   twitch@gmail.com
                 </div>
                 <div className="Wallet__verification--phone">
-                  <img src="https://www.dlf.pt/png/big/1/13075_false-png.png" alt="unverified" width={20} height={20} /> 
+                  <img src="https://www.dlf.pt/png/big/1/13075_false-png.png" alt="unverified" width={20} height={20} />
                   no phone verified
                 </div>
                 <div className="Wallet__verification--id">
