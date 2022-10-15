4a0b4a116b608906bf66c70e43e9f53c55c01546
kostic0510 <jackkostic0510@gmail.com>
2022-10-15 19:09:24 +0900

    message

--- src/components/Wallet/index.js
+++ src/components/Wallet/index.js
@@ -1,12 +1,7 @@
 import {
   Address,
-  ScriptHash,
   TransactionUnspentOutput,
   Value,
-  NetworkInfo,
-  EnterpriseAddress,
-  StakeCredential,
-  PlutusScript,
 } from '@emurgo/cardano-serialization-lib-asmjs';
 /* eslint-disable unicorn/numeric-separators-style */
 import {
@@ -16,7 +11,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Wallet.css';
 // eslint-disable-next-line import/no-unresolved, no-unused-vars
-import { blake2b } from 'blakejs';
 import useUserInfo from '../../hooks/user/userInfo';
 
 // eslint-disable-next-line import/no-unresolved, unicorn/prefer-module
@@ -24,76 +18,23 @@ const { Buffer } = require('buffer/');
 // eslint-disable-next-line import/no-unresolved, unicorn/prefer-module, no-unused-vars
 const blake = require('blakejs');
 
-/**
-     * Protocol parameters
-     * @type {{
- * keyDeposit: string,
- * coinsPerUtxoWord: string,
- * minUtxo: string,
- * poolDeposit: string,
- * maxTxSize: number,
- * priceMem: number,
- * maxValSize: number,
- * linearFee: {minFeeB: string, minFeeA: string}, priceStep: number
- * }}
- */
-// eslint-disable-next-line unicorn/prevent-abbreviations, no-unused-vars
-const protocolParams = {
-  linearFee: {
-    minFeeA: '44',
-    minFeeB: '155381',
-  },
-  minUtxo: '34482',
-  poolDeposit: '500000000',
-  keyDeposit: '2000000',
-  maxValSize: 5000,
-  maxTxSize: 16384,
-  priceMem: 0.0577,
-  priceStep: 0.0000721,
-  coinsPerUtxoWord: '34482',
-};
-/**
-   * Generate address from the plutus contract cborhex
-   */
-// eslint-disable-next-line no-unused-vars
-const generateScriptAddress = (state) => {
-  // cborhex of the alwayssucceeds.plutus
-  // const cborhex = "4e4d01000033222220051200120011";
-  // const cbor = Buffer.from(cborhex, "hex");
-  // const blake2bhash = blake.blake2b(cbor, 0, 28);
-
-  // eslint-disable-next-line no-unused-vars
-  const script = PlutusScript.from_bytes(Buffer.from(state.plutusScriptCborHex, 'hex'));
-  // const blake2bhash = blake.blake2b(script.to_bytes(), 0, 28);
-  const blake2bhash = '67f33146617a5e61936081db3b2117cbf59bd2123748f58ac9678656';
-  const scripthash = ScriptHash.from_bytes(Buffer.from(blake2bhash, 'hex'));
-
-  const cred = StakeCredential.from_scripthash(scripthash);
-  const networkId = NetworkInfo.testnet().network_id();
-  const baseAddr = EnterpriseAddress.new(networkId, cred);
-  const addr = baseAddr.to_address();
-  const addrBech32 = addr.to_bech32();
-
-  // hash of the address generated from script
-  // eslint-disable-next-line no-console
-  console.log(Buffer.from(addr.to_bytes(), 'utf8').toString('hex'));
-
-  // hash of the address generated using cardano-cli
-  const ScriptAddress = Address
-    .from_bech32('addr_test1wpnlxv2xv9a9ucvnvzqakwepzl9ltx7jzgm53av2e9ncv4sysemm8');
-  // eslint-disable-next-line no-console
-  console.log(Buffer.from(ScriptAddress.to_bytes(), 'utf8').toString('hex'));
-
-  // eslint-disable-next-line no-console
-  console.log(ScriptAddress.to_bech32());
-  // eslint-disable-next-line no-console
-  console.log(addrBech32);
-};
-
-// the button to connect a cardano wallet.
-// has 3 states: no wallet found, 1 wallet found, multiple wallets found
 function ButtonConnectWallet(state, pollWallets, setState, refreshData) {
-  const { wallets, whichWalletSelected } = state;
+  const {
+    wallets, whichWalletSelected, balance, usedAddress,
+  } = state;
+  // formating useraddress
+  let start5;
+  let middle5;
+  let last5;
+  let joined;
+
+  if (usedAddress) {
+    start5 = usedAddress.slice(0, 10);
+    middle5 = '.....';
+    last5 = usedAddress.slice(93, 103);
+    joined = start5 + middle5 + last5;
+  }
+
   // eslint-disable-next-line sonarjs/no-unused-collection
   const buttons = [];
   // eslint-disable-next-line no-console
@@ -108,13 +49,41 @@ function ButtonConnectWallet(state, pollWallets, setState, refreshData) {
   }
   if (wallets?.length === 1) {
     return (
-      <button
-        onClick={() => refreshData()}
-      >
-        Connect
-        {' '}
-        {wallets[0]}
-      </button>
+      (
+        joined === undefined ? (
+          <div className="c_button">
+            <button
+              onClick={() => refreshData()}
+              className="hover"
+            >
+              Connect
+              {' '}
+              {wallets[0]}
+            </button>
+          </div>
+        ) : (
+          <div className="c_button">
+            <button
+              onClick={() => window.location.reload()}
+              className="hover"
+            >
+              Disconnect
+              {' '}
+              {wallets[0]}
+            </button>
+            <p>
+              balance:
+              &nbsp;&nbsp;
+              {balance}
+            </p>
+            <p>
+              Address:
+              &nbsp;&nbsp;
+              {joined}
+            </p>
+          </div>
+        )
+      )
     );
   }
   // eslint-disable-next-line unicorn/explicit-length-check
@@ -124,6 +93,7 @@ function ButtonConnectWallet(state, pollWallets, setState, refreshData) {
         buttons.push(
           <button
             onClick={() => refreshData()}
+            className="hover"
           >
             Connect
             {' '}
@@ -136,6 +106,7 @@ function ButtonConnectWallet(state, pollWallets, setState, refreshData) {
             onClick={() => {
               setState((previous) => ({ ...previous, whichWalletSelected: wallet }));
             }}
+            className="hover"
           >
             Connect
             {' '}
@@ -162,13 +133,6 @@ function Wallet() {
   const handleClickSeeCreation = () => {
     navigate('/creations');
   };
-
-  // wallet connection state
-  // const [whichWalletSelected, setWichWalletSelected] = useState();
-  // const [walletFound, setWalletFound] = useState(false);
-  // const [walletIsEnabled, setWalletIsEnabled] = useState(false);
-  // const [walletAPIVersion, setWalletAPIVersion] = useState();
-  // const [walletName, setWalletName] = useState();
 
   const [state, setState] = useState({
     selectedTabId: '1',
@@ -208,11 +172,6 @@ function Wallet() {
 
   });
 
-  /**
-           * When the wallet is connect it returns the connector which is
-           * written to this API variable and all the other operations
-           * run using this API object
-           */
   let API;
 
   /**
@@ -488,7 +447,6 @@ function Wallet() {
   const refreshData = async () => {
     // eslint-disable-next-line no-console
     console.log('refreshing data');
-    generateScriptAddress(state);
 
     try {
       const walletIsFound = checkIfWalletFound();
@@ -580,55 +538,21 @@ function Wallet() {
     // refreshData();
   };
 
-  /**
-   * Handles the tab selection on the user form
-   * @param tabId
-   */
-  // eslint-disable-next-line no-unused-vars
-  const handleTabId = (tabId) => setState(
-    (previous) => previous === { ...previous, selectedTabId: tabId },
-  );
-
-  /**
-   * Handles the radio buttons on the form that
-   * let the user choose which wallet to work with
-   * @param obj
-   */
-  // eslint-disable-next-line no-unused-vars
-  const handleWalletSelect = (object) => {
-    const whichWalletSelected = object;
-    setState((previous) => previous === {
-      ...previous,
-      whichWalletSelected,
-    });
-    refreshData();
-  };
-
-  useEffect(() => {
-    // pollWallets();
-    if (state.whichWalletSelected) {
-      refreshData();
-    }
-  }, [state.whichWalletSelected]);
-
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('state', state);
-  }, [state]);
-
   useEffect(() => {
     pollWallets();
-    // refreshData();
   }, []);
 
   return (
     <div>
       <div className="wallet-div">
         <div className="inner-heading">
-          <b>My Wallet</b>
+          <div><b>My Wallet</b></div>
           {state.wallets?.length
-            ? ButtonConnectWallet(state, pollWallets, setState, refreshData) : null}
-          <button>Switch wallet</button>
+            ? ButtonConnectWallet(state, pollWallets, setState, refreshData)
+            : (
+              <p>First install your wallet.</p>
+            )}
+          <div><button>Switch wallet</button></div>
         </div>
         <div className="front">
           <div className="front__bkg-photo" />
