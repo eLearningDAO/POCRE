446eb9376d047e2089d47eb74b9ac52c1036184f
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-10-19 03:22:50 +0500

    show user their litigations

--- app/web-frontend/src/components/Litigation/Dashboard/index.js
+++ app/web-frontend/src/components/Litigation/Dashboard/index.js
@@ -1,20 +1,111 @@
-import { Button, Grid, Typography } from '@mui/material';
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import moment from 'moment';
+import {
+  Button, Grid, Typography,
+} from '@mui/material';
+import React, { useEffect } from 'react';
 import ApproveIcon from '../../../assets/approve-icon.png';
-import UserImage2 from '../../../assets/top-learner-user.png';
-import UserImage from '../../../assets/user-image-2.jpeg';
 import WithdrawIcon from '../../../assets/withdraw-icon.png';
 import './index.css';
+import Loader from '../../uicore/Loader';
+import useDashboard from './useDashboard';
+
+function LitigatedItemRow({
+  title = 'I want to reclaim',
+  issuer = {
+    name: 'bob',
+    walletAddress: '73gd8egv',
+  },
+  assumedAuthor = {
+    name: 'bob',
+    walletAddress: '73gd8egv',
+  },
+  startDate = '27/10/2022',
+  endDate = '27/10/2022',
+  mode = 'info',
+  // mode = info
+  totalJuryMembers = 5,
+  // mode = litigate
+  canWithdraw = false,
+  canAccept = false,
+  onWithdraw = () => {},
+  onAccept = () => {},
+  // mode = closed
+  winner = {
+    name: 'bob',
+    walletAddress: '73gd8egv',
+  },
+}) {
+  return (
+    <tr className="litigationBoxBody">
+      <td>{title}</td>
+      <td className="responsive">
+        <div className="litigationUser">
+          <div className="litigationUserImage">
+            <img src={`https://i.pravatar.cc/50?img=${Math.random()}`} alt="" />
+          </div>
+          <div className="litigationUserText">
+            <h6>{issuer?.name}</h6>
+            {issuer?.walletAddress?.slice(1, 8)}
+          </div>
+        </div>
+      </td>
+      <td className="responsive">
+        <div className="litigationUser">
+          <div className="litigationUserImage">
+            <img src={`https://i.pravatar.cc/50?img=${Math.random()}`} alt="" />
+          </div>
+          <div className="litigationUserText">
+            <h6>{assumedAuthor?.name}</h6>
+            {assumedAuthor?.walletAddress?.slice(1, 8)}
+          </div>
+        </div>
+      </td>
+      <td className="responsive">{startDate}</td>
+      <td className="responsive">{endDate}</td>
+      <td className="responsive">
+        {mode === 'info' && totalJuryMembers}
+        {mode === 'litigate'
+        && (
+          (canWithdraw && (
+          <Button onClick={onWithdraw} className="withdrawButton">
+            <img src={WithdrawIcon} alt="withdraw" />
+            Withdraw
+          </Button>
+          ))
+          || (canAccept && (
+          <Button onClick={onAccept} className="approveButton">
+            <img src={ApproveIcon} alt="withdraw" />
+            Approve
+          </Button>
+          ))
+        )}
+        {mode === 'closed'
+        && (
+        <div className="litigationUser">
+          <div className="litigationUserImage">
+            <img src={`https://i.pravatar.cc/50?img=${Math.random()}`} alt="" />
+          </div>
+          <div className="litigationUserText">
+            <h6>{winner?.name}</h6>
+            {winner?.walletAddress?.slice(1, 8)}
+          </div>
+        </div>
+        )}
+      </td>
+    </tr>
+  );
+}
 
 function Litigation() {
   // get userInfo from the globale state with help of zustand store hook !
   // const user = useUserInfo((s) => s.user);
+  const { fetchLitigations, litigations, isFetchingLitigations } = useDashboard();
 
-  const navigate = useNavigate();
-  const approve = () => {
-    navigate('/litigation/closed');
-  };
+  useEffect(() => {
+    fetchLitigations();
+  }, []);
+
+  if (isFetchingLitigations) return <Loader />;
 
   return (
     <Grid container className="Litigation">
@@ -24,240 +115,130 @@ function Litigation() {
         </Typography>
       </Grid>
 
-      <Grid item xs={12} style={{ marginTop: '10px' }}>
-        <Typography className="inviationSectionTitle" variant="h6">
-          Opened
-        </Typography>
-      </Grid>
+      {litigations?.results?.opening?.length > 0 && (
+      <>
+        <Grid item xs={12} style={{ marginTop: '10px' }}>
+          <Typography className="inviationSectionTitle" variant="h6">
+            Waiting authorship recognition
+          </Typography>
+        </Grid>
 
-      <table className="litigationBox">
-        <tr className="litigationBoxHeader">
-          <th>Creation Title</th>
-          <th className="responsive">Claimer</th>
-          <th className="responsive">Assumed Author</th>
-          <th className="responsive">Litigation Start</th>
-          <th className="responsive">Deadline</th>
-          <th className="responsive">Availabel jurors</th>
-        </tr>
+        <table className="litigationBox">
+          <tr className="litigationBoxHeader">
+            <th>Creation Title</th>
+            <th className="responsive">Claimer</th>
+            <th className="responsive">Assumed Author</th>
+            <th className="responsive">Litigation Start</th>
+            <th className="responsive">Deadline</th>
+            <th className="responsive">Total jurors</th>
+          </tr>
 
-        <tr className="litigationBoxBody">
-          <td>I want reclaim my authorship of this media</td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage2} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">01/05/2022</td>
-          <td className="responsive">01/07/2022</td>
-          <td className="responsive">9/10</td>
-        </tr>
+          {litigations?.results?.opening?.map((x) => (
+            <LitigatedItemRow
+              key={x}
+              title={x?.litigation_title}
+              claimer={{
+                name: x?.issuer?.user_name,
+                walletAddress: x?.issuer?.wallet_address,
+              }}
+              assumedAuthor={{
+                name: x?.assumed_author?.user_name,
+                walletAddress: x?.assumed_author?.wallet_address,
+              }}
+              startDate={moment(x.litigation_start).format('DD/MM/YYYY')}
+              endDate={moment(x.litigation_end).format('DD/MM/YYYY')}
+              mode="info"
+              totalJuryMembers={x?.invitations?.length}
+            />
+          ))}
+        </table>
+      </>
+      )}
 
-        <tr className="litigationBoxBody">
-          <td>I want reclaim my authorship of this media</td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage2} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">01/05/2022</td>
-          <td className="responsive">01/07/2022</td>
-          <td className="responsive">9/10</td>
-        </tr>
-      </table>
+      {litigations?.results?.openedAgainstMe?.length > 0 && (
+      <>
+        <Grid item xs={12} style={{ marginTop: '20px' }}>
+          <Typography className="inviationSectionTitle" variant="h6">
+            Opened against me
+          </Typography>
+        </Grid>
 
-      <Grid item xs={12} style={{ marginTop: '20px' }}>
-        <Typography className="inviationSectionTitle" variant="h6">
-          In progress
-        </Typography>
-      </Grid>
+        <table className="litigationBox">
+          <tr className="litigationBoxHeader">
+            <th>Creation Title</th>
+            <th className="responsive">Claimer</th>
+            <th className="responsive">Assumed Author</th>
+            <th className="responsive">Litigation Start</th>
+            <th className="responsive">Deadline</th>
+            <th className="responsive">Actions</th>
+          </tr>
 
-      <table className="litigationBox">
-        <tr className="litigationBoxHeader">
-          <th>Creation Title</th>
-          <th className="responsive">Claimer</th>
-          <th className="responsive">Assumed Author</th>
-          <th className="responsive">Litigation Start</th>
-          <th className="responsive">Deadline</th>
-          <th className="responsive">Availabel jurors</th>
-        </tr>
+          {litigations?.results?.openedAgainstMe?.map((x, index) => (
+            <LitigatedItemRow
+              key={x}
+              title={x?.litigation_title}
+              claimer={{
+                name: x?.issuer?.user_name,
+                walletAddress: x?.issuer?.wallet_address,
+              }}
+              assumedAuthor={{
+                name: x?.assumed_author?.user_name,
+                walletAddress: x?.assumed_author?.wallet_address,
+              }}
+              startDate={moment(x.litigation_start).format('DD/MM/YYYY')}
+              endDate={moment(x.litigation_end).format('DD/MM/YYYY')}
+              mode="litigate"
+              canWithdraw={index % 2 === 0}
+              canAccept={index % 2 !== 0}
+            />
+          ))}
+        </table>
+      </>
+      )}
 
-        <tr className="litigationBoxBody">
-          <td>I want reclaim my authorship of this media</td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage2} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">01/05/2022</td>
-          <td className="responsive">01/07/2022</td>
-          <td className="responsive">
-            <Button className="withdrawButton">
-              <img src={WithdrawIcon} alt="withdraw" />
-              Withdraw
-            </Button>
-          </td>
-        </tr>
+      {litigations?.results?.closed?.length > 0 && (
+      <>
+        <Grid item xs={12} style={{ marginTop: '20px' }}>
+          <Typography className="inviationSectionTitle" variant="h6">
+            Closed
+          </Typography>
+        </Grid>
 
-        <tr className="litigationBoxBody">
-          <td>I want reclaim my authorship of this media</td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage2} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">01/05/2022</td>
-          <td className="responsive">01/07/2022</td>
-          <td className="responsive">
-            <Button onClick={approve} className="approveButton">
-              <img src={ApproveIcon} alt="withdraw" />
-              Approve
-            </Button>
-          </td>
-        </tr>
-      </table>
+        <table className="litigationBox">
+          <tr className="litigationBoxHeader">
+            <th>Creation Title</th>
+            <th className="responsive">Claimer</th>
+            <th className="responsive">Assumed Author</th>
+            <th className="responsive">Litigation Start</th>
+            <th className="responsive">Deadline</th>
+            <th className="responsive">Winner</th>
+          </tr>
 
-      <Grid item xs={12} style={{ marginTop: '20px' }}>
-        <Typography className="inviationSectionTitle" variant="h6">
-          Closed
-        </Typography>
-      </Grid>
+          {litigations?.results?.closed?.map((x) => (
+            <LitigatedItemRow
+              key={`${x}`}
+              title={x?.litigation_title}
+              claimer={{
+                name: x?.issuer?.user_name,
+                walletAddress: x?.issuer?.wallet_address,
+              }}
+              assumedAuthor={{
+                name: x?.assumed_author?.user_name,
+                walletAddress: x?.assumed_author?.wallet_address,
+              }}
+              startDate={moment(x.litigation_start).format('DD/MM/YYYY')}
+              endDate={moment(x.litigation_end).format('DD/MM/YYYY')}
+              mode="closed"
+              winner={{
+                name: x?.winner?.user_name,
+                walletAddress: x?.winner?.wallet_address,
+              }}
+            />
+          ))}
 
-      <table className="litigationBox">
-        <tr className="litigationBoxHeader">
-          <th>Creation Title</th>
-          <th className="responsive">Claimer</th>
-          <th className="responsive">Assumed Author</th>
-          <th className="responsive">Litigation Start</th>
-          <th className="responsive">Deadline</th>
-          <th className="responsive">Availabel jurors</th>
-        </tr>
-
-        <tr className="litigationBoxBody">
-          <td>I want reclaim my authorship of this media</td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage2} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">01/05/2022</td>
-          <td className="responsive">01/07/2022</td>
-          <td className="responsive">9/10</td>
-        </tr>
-
-        <tr className="litigationBoxBody">
-          <td>I want reclaim my authorship of this media</td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">
-            <div className="litigationUser">
-              <div className="litigationUserImage">
-                <img src={UserImage2} alt="" />
-              </div>
-              <div className="litigationUserText">
-                <h6>Bobi</h6>
-                0X 3A5 EE2
-              </div>
-            </div>
-          </td>
-          <td className="responsive">01/05/2022</td>
-          <td className="responsive">01/07/2022</td>
-          <td className="responsive">9/10</td>
-        </tr>
-      </table>
+        </table>
+      </>
+      )}
     </Grid>
   );
 }
