c132827b5ab58711cf264096384f62e3ad16a30b
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-10-24 00:18:09 +0500

    show voted status on litigations

--- app/web-frontend/src/components/Litigation/Home/index.js
+++ app/web-frontend/src/components/Litigation/Home/index.js
@@ -36,17 +36,19 @@ function LitigationCard({
   onWithdraw = () => {},
   onAccept = () => {},
   // mode = closed
-  canRedeem = true,
-  isRedeemed = true,
-  lostClaim = false,
-  votedInFavour = null,
-  votedInOpposition = null,
-  notVoted = null,
-  onRedeem = () => {},
   winner = {
     name: 'bob',
     walletAddress: '73gd8egv',
   },
+  canRedeem = true,
+  isRedeemed = true,
+  onRedeem = () => {},
+  lostClaim = false,
+  // mode = closed or mode = toJudge
+  votedInFavour = null,
+  votedInOpposition = null,
+  notVoted = null,
+
 }) {
   return (
     <div className="litigation-card1">
@@ -63,6 +65,8 @@ function LitigationCard({
           <img src={`https://i.pravatar.cc/50?img=${Math.random()}`} alt="" />
           <h4>{assumedAuthor.name}</h4>
           <p>{assumedAuthor.walletAddress?.slice(1, 8)}</p>
+          {mode === 'closed' && winner.name === assumedAuthor.name
+          && <span className="litigation-winner-label">Winner</span>}
         </div>
         <h1>vs</h1>
         <div className="litigation-card-author">
@@ -118,27 +122,30 @@ function LitigationCard({
             label="You lost the claim"
           />
         )}
-        {mode === 'closed' && !canRedeem
+        {((mode === 'closed' && !canRedeem
         && (
           votedInFavour
           || votedInOpposition
           || notVoted
-        )
+        )) || mode === 'toJudge')
         && (
           (votedInFavour && (
             <Chip
+              style={{ fontSize: '14px' }}
               className="color-white bg-green"
               label="Voted in favor"
             />
           ))
           || (votedInOpposition && (
             <Chip
+              style={{ fontSize: '14px' }}
               className="color-white bg-red"
               label="Voted in opposition"
             />
           ))
          || (notVoted && (
          <Chip
+           style={{ fontSize: '14px' }}
            className="color-white bg-black"
            label="Vote not casted"
          />
@@ -161,6 +168,8 @@ function LitigationCard({
         <a className="litigation-details-link" href={`/litigation/${id}`}>
           <h4>
             View Details
+            {' '}
+            {mode === 'toJudge' && notVoted ? 'To Vote' : ''}
           </h4>
           {' '}
           <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z" /></svg>
@@ -301,6 +310,7 @@ function Litigation() {
                   (activeLitigation === 'opening' && 'info')
                   || (activeLitigation === 'openedAgainstMe' && 'litigate')
                   || (activeLitigation === 'closed' && 'closed')
+                  || (activeLitigation === 'toJudge' && 'toJudge')
                 }
                 notVoted={
                   x?.toJudge
