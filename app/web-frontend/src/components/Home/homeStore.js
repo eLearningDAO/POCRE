4ec7bf34816a5058b90b9660fae914a07b9918c5
shujaat <shujaat.dev@taazaa.com>
2022-10-24 16:19:44 +0500

    fixed homestore eslint issue

--- app/web-frontend/src/components/Home/homeStore.js
+++ app/web-frontend/src/components/Home/homeStore.js
@@ -4,26 +4,25 @@ const trendingUrl = 'https://pocre-api.herokuapp.com/v1/creations?is_trending=tr
 const topAuthorUrl = 'https://pocre-api.herokuapp.com/v1/users?top_authors=true';
 const materialsUrl = 'https://pocre-api.herokuapp.com/v1/materials?is_recognized=true&is_claimed=false';
 
-
 const useStore = create((set) => ({
-    trendingList: [],
-    topAuthorList: [],
-    materialList: [],
-    fetchAuthor: async () => {
-        const response = await fetch(topAuthorUrl);
-        const author = await response.json();
-        set({ topAuthorList: author.results });
-    },
-    fetchMaterial: async () => {
-        const response = await fetch(materialsUrl);
-        const material = await response.json();
-        set({ materialList: material.results });
-    },
-    fetchTrending: async () => {
-        const response = await fetch(trendingUrl);
-        const trending = await response.json();
-        set({ trendingList: trending.results });
-    },
+  trendingList: [],
+  topAuthorList: [],
+  materialList: [],
+  fetchAuthor: async () => {
+    const response = await fetch(topAuthorUrl);
+    const author = await response.json();
+    set({ topAuthorList: author.results });
+  },
+  fetchMaterial: async () => {
+    const response = await fetch(materialsUrl);
+    const material = await response.json();
+    set({ materialList: material.results });
+  },
+  fetchTrending: async () => {
+    const response = await fetch(trendingUrl);
+    const trending = await response.json();
+    set({ trendingList: trending.results });
+  },
 }));
 
 export default useStore;
