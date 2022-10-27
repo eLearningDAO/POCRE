f0038b00dba2212ab1bd2205d38d7199cf188429
shujaat <shujaat.dev@taazaa.com>
2022-10-27 14:49:27 +0500

    eslint issue fixed

--- app/web-frontend/src/pages/Wallet/detail/walletDetail.js
+++ app/web-frontend/src/pages/Wallet/detail/walletDetail.js
@@ -6,29 +6,28 @@ import WalletDisplay from './walletDisplay';
 import CameraIcon from '../../../assets/svgs/cameraIcon.svg';
 
 function WalletDetail() {
-    const [isDetailEdit, setDetailEdit] = useState(false);
+  const [isDetailEdit, setDetailEdit] = useState(false);
 
-    return (
-        <div className="wallet-detail-container">
-            <div className="wallete-detail-left-container">
-                <div className="front-face-photo">
-                    <img src={profileImg} alt="alt" />
-                    {
-                        isDetailEdit && (
-                            <div className="edit-camera">
-                                <img src={CameraIcon} alt="camera" />
-                            </div>
-                        )
-                    }
-                </div>
-
-                <span className="author-collection">Author of 10 collections</span>
+  return (
+    <div className="wallet-detail-container">
+      <div className="wallete-detail-left-container">
+        <div className="front-face-photo">
+          <img src={profileImg} alt="alt" />
+          {isDetailEdit && (
+            <div className="edit-camera">
+              <img src={CameraIcon} alt="camera" />
             </div>
-            {
-                isDetailEdit ? <WalletDetailEdit />
-                    : <WalletDisplay setDetailEdit={setDetailEdit} />
-            }
+          )}
         </div>
-    );
+
+        <span className="author-collection">Author of 10 collections</span>
+      </div>
+      {isDetailEdit ? (
+        <WalletDetailEdit />
+      ) : (
+        <WalletDisplay setDetailEdit={setDetailEdit} />
+      )}
+    </div>
+  );
 }
 export default WalletDetail;
