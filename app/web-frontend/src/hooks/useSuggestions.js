94b1bd3e58af41937534051ed7e8729a0013b080
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-11-05 00:26:39 +0500

    added react query to suggestions hook

--- app/web-frontend/src/hooks/useSuggestions.js
+++ app/web-frontend/src/hooks/useSuggestions.js
@@ -1,22 +1,11 @@
 import { Creation, Tag, User } from 'api/requests';
-import { useCallback, useState } from 'react';
-
-let debounceInterval = null;
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 const searchMap = {
-  users: {
-    method: User.getAll,
-    searchField: 'user_name',
-  },
-  creations: {
-    method: Creation.getAll,
-    searchField: 'creation_title',
-  },
-  tags: {
-    method: Tag.getAll,
-    searchField: 'tag_name',
-  },
-  // add more search maps here when needed
+  users: { method: User.getAll, searchField: 'user_name' },
+  creations: { method: Creation.getAll, searchField: 'creation_title' },
+  tags: { method: Tag.getAll, searchField: 'tag_name' },
 };
 
 const useSuggestions = ({
@@ -28,72 +17,39 @@ const useSuggestions = ({
     is_claimable: true,
   },
 }) => {
-  const [suggestionsStatus, setSuggestionsStatus] = useState({
-    success: false,
-    error: null,
+  const [searchText, setSearchText] = useState(null);
+
+  const { data, isError, isSuccess } = useQuery({
+    queryKey: [`suggestion-${search}-${searchText}`],
+    queryFn: async () => {
+      const suggestions = await searchMap[search].method(`query=${searchText}&limit=50&search_fields[]=${searchMap[search].searchField}`);
+
+      return suggestions
+        ?.results
+        ?.filter(
+          (x) => (
+            filterSuggestion
+              ? x[searchMap[search].searchField].trim() !== filterSuggestion.trim()
+              : x),
+        )?.filter(
+          (x) => (
+            search === 'creations' && config.is_claimable ? x.is_claimable : x
+          ),
+        );
+    },
+    staleTime: 100_000, // delete cached data after 100 seconds
+    enabled: !!searchText,
   });
-  const [suggestions, setSuggestions] = useState([]);
-
-  // get suggestions
-  const findSuggestions = useCallback(async (searchText = '') => {
-    try {
-      const response = await searchMap[search].method(`query=${searchText}&search_fields[]=${searchMap[search].searchField}`);
-
-      setSuggestionsStatus({
-        success: true,
-        error: null,
-      });
-      setTimeout(() => setSuggestionsStatus({
-        success: false,
-        error: null,
-      }), 3000);
-
-      return response;
-    } catch {
-      setSuggestionsStatus({
-        success: false,
-        error: 'Failed to get suggestions',
-      });
-    }
-    return null;
-  }, []);
 
   // get suggestion on input change
-  const handleSuggestionInputChange = async (event) => {
-    if (debounceInterval) clearTimeout(debounceInterval);
-
-    debounceInterval = await setTimeout(async () => {
-      const value = event.target.value.trim();
-      if (!value) return;
-
-      const temporarySuggestions = await findSuggestions(value);
-
-      const validSuggestions = [];
-
-      temporarySuggestions?.results
-        ?.filter(
-          (x) => (filterSuggestion
-            ? x[searchMap[search].searchField].trim() !== filterSuggestion.trim()
-            : x),
-        )
-        ?.map(
-          (x) => validSuggestions.findIndex(
-            (y) => y[searchMap[search].searchField] === x[searchMap[search].searchField],
-          ) <= -1
-          && validSuggestions.push(x),
-        );
-
-      setSuggestions([
-        ...suggestions,
-        // this filter is only applied when searching for creation suggestions
-        ...validSuggestions.filter((x) => (search === 'creations' && config.is_claimable ? x.is_claimable : x)),
-      ]);
-    }, 500);
-  };
+  const handleSuggestionInputChange = (event) => setSearchText(event.target.value.trim());
 
   return {
-    suggestions,
-    suggestionsStatus,
+    suggestions: data,
+    suggestionsStatus: {
+      success: isSuccess,
+      error: isError ? 'Failed to get suggestions' : null,
+    },
     handleSuggestionInputChange,
   };
 };
