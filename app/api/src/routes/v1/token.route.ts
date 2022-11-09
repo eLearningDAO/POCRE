4c399f5326f55d497d9cba80d434b860a102bab5
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-11-09 21:22:00 +0500

    added swagger docs for token endpoints

--- app/api/src/routes/v1/token.route.ts
+++ app/api/src/routes/v1/token.route.ts
@@ -8,3 +8,40 @@ const router = express.Router();
 router.route('/').post(validate(tokenValidation.createToken), tokenController.createToken);
 
 export default router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Token
+ *   description: Token management and retrieval
+ */
+
+/**
+ * @swagger
+ * /token:
+ *   post:
+ *     summary: Create a token
+ *     description: Creates a new jwt token.
+ *     tags: [Token]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - payload
+ *             properties:
+ *               payload:
+ *                 type: string
+ *                 description: the string to encode as jwt
+ *             example:
+ *                payload: 1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Token'
+ */
