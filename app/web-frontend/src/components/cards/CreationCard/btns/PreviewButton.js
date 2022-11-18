79057ebc0e438e7e879421d770d1fa981e5263da
shujaat <shujaat.dev@taazaa.com>
2022-11-18 17:19:52 +0500

    change the icon

--- app/web-frontend/src/components/cards/CreationCard/btns/PreviewButton.js
+++ app/web-frontend/src/components/cards/CreationCard/btns/PreviewButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import PreviewIcon from 'assets/svgs/preview.svg';
+import PreviewIcon from 'assets/images/previewicons.png';
 
 function PreviewButton({ onClick }) {
   return (
     <Button className="collection-card-action-btn" onClick={onClick}>
-      <img src={PreviewIcon} alt="" />
+      <img src={PreviewIcon} alt="" width={26} />
     </Button>
   );
 }
