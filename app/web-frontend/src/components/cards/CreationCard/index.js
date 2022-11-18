df10687c384da48fe5c6cb2b7a601fd26d20cd31
shujaat <shujaat.dev@taazaa.com>
2022-11-18 14:35:56 +0500

    added share media modal in recogination detail page

--- app/web-frontend/src/components/cards/CreationCard/index.js
+++ app/web-frontend/src/components/cards/CreationCard/index.js
@@ -134,10 +134,6 @@ function CollectionCard({
     if (onDeleteClick) await onDeleteClick();
   };
 
-  const handleSocialMediaSharing = () => {
-    setShowSocialMediaSharePreview(true);
-  };
-
   return (
     <>
       {showDeleteConfirmation && (
@@ -357,7 +353,7 @@ function CollectionCard({
                 onEditClick={onEditClick}
               />
               <ShareButton
-                onClick={handleSocialMediaSharing}
+                onClick={() => setShowSocialMediaSharePreview(true)}
               />
             </Grid>
           </Grid>
