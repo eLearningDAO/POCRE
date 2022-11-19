102b721a933726dd358a7aeb1c00d006d7cd784d
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-11-19 21:25:36 +0500

    added swagger docs for auth endpoint

--- app/api/src/routes/v1/auth.route.ts
+++ app/api/src/routes/v1/auth.route.ts
@@ -9,3 +9,38 @@ router.route('/login').post(validate(authValidation.createSession), authControll
 
 export default router;
 
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Auth and session management
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Creates a new auth session for a user
+ *     description: Creates a new jwt for a user using wallet address. If user is not present it will create a new one and return that as response
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - wallet_address
+ *             properties:
+ *               wallet_address:
+ *                 type: string
+ *             example:
+ *                wallet_address: 89y98gf37g87cg26dr63rf7dt8734t2df683xvc
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Session'
+ */
