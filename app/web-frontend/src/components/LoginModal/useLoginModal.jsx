2fe90547acfbcf8f0c2ffcc8e9a569f943af406a
huzaifa-99 <huzaifarasheed900@gmail.com>
2022-12-13 00:17:09 +0500

    show hashed wallet address when logging in

--- app/web-frontend/src/components/LoginModal/useLoginModal.jsx
+++ app/web-frontend/src/components/LoginModal/useLoginModal.jsx
@@ -5,9 +5,34 @@ import authUser from 'utils/helpers/authUser';
 import { getAvailableWallets, getWalletAddress } from 'utils/helpers/wallet';
 
 const useLoginModal = () => {
+  const [walletAddressError, setWalletAddressError] = useState(null);
+  const [loadingWalletAddress, setLoadingWalletAddress] = useState(null);
+  const [selectedWalletAddressOriginal, setSelectedWalletAddressOriginal] = useState(null);
+  const [selectedWalletAddressHashed, setSelectedWalletAddressHashed] = useState(null);
   const [availableWallets, setAvailableWallets] = useState([]);
 
   const getWalletsList = () => setAvailableWallets(getAvailableWallets());
+
+  const getSelectedWalletAddress = async (wallet) => {
+    try {
+      setSelectedWalletAddressHashed(null);
+      setLoadingWalletAddress(true);
+      setWalletAddressError(null);
+
+      // get wallet address from browser
+      const walletAddress = await getWalletAddress(wallet);
+
+      // if wallet address is not found
+      if (!walletAddress) { setWalletAddressError('Cannot login with this wallet!'); return; }
+
+      setSelectedWalletAddressOriginal(walletAddress);
+      setSelectedWalletAddressHashed(`${walletAddress.slice(0, 10)}..........${walletAddress.slice(-10)}`);
+    } catch {
+      setWalletAddressError('Cannot login with this wallet!'); return;
+    } finally {
+      setLoadingWalletAddress(false);
+    }
+  };
 
   const {
     mutate: loginWithWallet,
@@ -15,13 +40,10 @@ const useLoginModal = () => {
     isSuccess: loginSuccess,
     isLoading: isLoggingIn,
   } = useMutation({
-    mutationFn: async ({ wallet }) => {
-      // get wallet address from browser
-      const walletAddress = await getWalletAddress(wallet);
-
+    mutationFn: async () => {
       // login with wallet
-      const response = await Auth.login({ wallet_address: walletAddress });
-      authUser.setUser(response.user);
+      const response = await Auth.login({ wallet_address: selectedWalletAddressOriginal });
+      authUser.setUser({ ...response.user, hashedWalletAddress: selectedWalletAddressHashed });
       authUser.setJWTToken(response.token);
     },
   });
@@ -30,6 +52,10 @@ const useLoginModal = () => {
     availableWallets,
     getWalletsList,
     loginWithWallet,
+    getSelectedWalletAddress,
+    selectedWalletAddressHashed,
+    loadingWalletAddress,
+    walletAddressError,
     isLoggingIn,
     loginStatus: {
       success: loginSuccess,
