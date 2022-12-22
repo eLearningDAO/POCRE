335d03c383f79cb09c0f670a062020dc544a2f70
UmairMukhtar <umairmukhtar27@gmail.com>
2022-12-22 08:55:01 -0800

    display correct image for authors

--- app/web-frontend/src/components/cards/TopAuthorCard/index.jsx
+++ app/web-frontend/src/components/cards/TopAuthorCard/index.jsx
@@ -8,7 +8,7 @@ function TopAuthorCard({ author, handleAuthorCardClick }) {
       {...accessibleOnClick(handleAuthorCardClick, author.user_id)}
     >
       <div className="topauthor-image">
-        <img alt="author" src={author.avatar} />
+        <img alt="author" src={author.image_url != null ? author.image_url : 'http://res.cloudinary.com/dia0ztihb/image/upload/v1669676677/Screenshot_2022-11-29_at_2.54.07_AM_xo13v8.png'} />
       </div>
       <div className="topauthor-body">
         <h6>
