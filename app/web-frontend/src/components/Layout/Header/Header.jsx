eb6893ba1efe9bdbfe116c69298c8687d2f3baca
UmairMukhtar <umairmukhtar27@gmail.com>
2022-12-26 08:49:15 -0800

    ligitations should only appear when user is logged in

--- app/web-frontend/src/components/Layout/Header/Header.jsx
+++ app/web-frontend/src/components/Layout/Header/Header.jsx
@@ -61,9 +61,13 @@ function HomeHeader({ displayNav = false }) {
               <li className={location.pathname === '/recognitions' ? 'activeSidebarMenu' : ''}>
                 <Link to="/recognitions">Recognitions</Link>
               </li>
-              {/* <li className={location.pathname === '/litigations' ? 'activeSidebarMenu' : ''}>
-                <Link to="/litigations">Litigations</Link>
-              </li> */}
+              {
+                loggedInUser && (
+                <li className={location.pathname === '/litigations' ? 'activeSidebarMenu' : ''}>
+                  <Link to="/litigations">Litigations</Link>
+                </li>
+                )
+                }
               <li className={location.pathname === '/wallet' ? 'activeSidebarMenu' : ''}>
                 <Link to="/wallet">Wallet</Link>
               </li>
