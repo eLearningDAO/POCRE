216b6ea8f427b85c4bc9d3798a0f0a798bcbd6a8
UmairMukhtar <umairmukhtar27@gmail.com>
2022-12-27 07:08:45 -0800

    use placeholder profile image from assets instead of hosted one

--- app/web-frontend/src/components/cards/TopAuthorCard/index.jsx
+++ app/web-frontend/src/components/cards/TopAuthorCard/index.jsx
@@ -1,4 +1,5 @@
 import './index.css';
+import ProfilePlaceholder from 'assets/images/profile-placeholder.png';
 import { accessibleOnClick } from 'utils/helpers/accessibleOnClick';
 
 function TopAuthorCard({ author, handleAuthorCardClick }) {
@@ -8,7 +9,7 @@ function TopAuthorCard({ author, handleAuthorCardClick }) {
       {...accessibleOnClick(handleAuthorCardClick, author.user_id)}
     >
       <div className="topauthor-image">
-        <img alt="author" src={author.image_url != null ? author.image_url : 'http://res.cloudinary.com/dia0ztihb/image/upload/v1669676677/Screenshot_2022-11-29_at_2.54.07_AM_xo13v8.png'} />
+        <img alt="author" src={author.image_url != null ? author.image_url : ProfilePlaceholder} />
       </div>
       <div className="topauthor-body">
         <h6>
