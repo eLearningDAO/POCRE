e2b8635adea7b71f5887ceb7b602165ebadb1110
huzaifa-99 <huzaifarasheed900@gmail.com>
2023-01-13 20:56:09 +0500

    updated IPFS content on creations step 3

--- app/web-frontend/src/pages/Creations/common/form/Steps/Three.jsx
+++ app/web-frontend/src/pages/Creations/common/form/Steps/Three.jsx
@@ -1,28 +1,17 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import {
-  Box, Button, Grid, TextField, Typography,
+  Box, Button, Grid, Typography,
 } from '@mui/material';
-import authUser from 'utils/helpers/authUser';
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import QRCode from 'qrcode';
 import PreviewIcon from 'assets/images/preview.png';
-import SaveIcon from 'assets/svgs/save.svg';
 import CreationPreview from 'components/previews/CreationPreview';
+import QRCode from 'qrcode';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authUser from 'utils/helpers/authUser';
 
 // get auth user
 const user = authUser.getUser();
-
-function dec2hex(dec) {
-  return dec.toString(16).padStart(2, '0');
-}
-
-function generateId(length_) {
-  const array = new Uint8Array((length_ || 40) / 2);
-  window.crypto.getRandomValues(array);
-  return [...array].map((element) => dec2hex(element)).join('');
-}
 
 export default function StepThree({
   creationId = '',
@@ -34,13 +23,7 @@ export default function StepThree({
 }) {
   const navigate = useNavigate();
 
-  const [dummyURL, setDummyURL] = useState('');
   const [showPreview, setShowPreview] = useState(false);
-
-  const generateDummyURL = () => {
-    setDummyURL(`https://ipfs.url.example.com/${generateId()}`);
-  };
-
   const [qrcodeBase64, setQrcodeBase64] = useState(null);
 
   const generateQRCodeBase64 = async (text) => {
@@ -61,23 +44,23 @@ export default function StepThree({
   return (
     <>
       {showPreview
-       && (
-       <CreationPreview
-         title={creationDraft?.title}
-         description={creationDraft?.description}
-         link={creationDraft?.source}
-         authorName={user?.user_name}
-         date={creationDraft?.date}
-         ipfsHash={creationDraft?.ipfsHash || 'Available after creation is created'}
-         materials={creationDraft?.materials?.map((x) => ({
-           title: x?.title,
-           fileType: x?.fileType,
-           link: x?.link,
-           authorName: x?.author,
-         }))}
-         onClose={() => setShowPreview(false)}
-       />
-       )}
+        && (
+          <CreationPreview
+            title={creationDraft?.title}
+            description={creationDraft?.description}
+            link={creationDraft?.source}
+            authorName={user?.user_name}
+            date={creationDraft?.date}
+            ipfsHash={creationDraft?.ipfsHash || 'Available after creation is created'}
+            materials={creationDraft?.materials?.map((x) => ({
+              title: x?.title,
+              fileType: x?.fileType,
+              link: x?.link,
+              authorName: x?.author,
+            }))}
+            onClose={() => setShowPreview(false)}
+          />
+        )}
       <Grid item xs={12}>
         <Grid container className="create-collection" justifyContent="flex-start" height="fit-content">
 
@@ -90,30 +73,13 @@ export default function StepThree({
             </Grid>
 
             <Grid md={qrcodeBase64 ? 4 : 3} xs={12} marginTop={{ xs: '12px', md: qrcodeBase64 ? '18px' : '36px' }} display="flex" flexDirection="row" alignItems="flex-start">
-              <Typography className="heading">Store optionally on IPFS</Typography>
+              <Typography className="heading">IPFS Hash</Typography>
             </Grid>
             <Grid md={qrcodeBase64 ? 8 : 9} xs={12} marginTop={{ xs: '12px', md: qrcodeBase64 ? '18px' : '36px' }}>
               <Grid display="flex" alignItems="flex-start" gap="12px">
-                {qrcodeBase64 && (
-                <Button padding="0" minWidth="0" onClick={generateDummyURL}>
-                  <img src={SaveIcon} height="36" width="36" alt="" />
-                </Button>
-                )}
-                <TextField
-                  value={dummyURL}
-                  variant="standard"
-                  InputProps={{
-                    disableUnderline: true,
-                  }}
-                  className="input input-dark"
-                  fullWidth
-                  placeholder="IPFS"
-                />
-                {!qrcodeBase64 && (
-                <Button padding="0" minWidth="0" onClick={generateDummyURL}>
-                  <img src={SaveIcon} height="36" width="36" alt="" />
-                </Button>
-                )}
+                <Box width="100%" className="bg-black color-white" style={{ wordWrap: 'break-word' }} padding="16px" borderRadius="12px" fontSize="16px">
+                  {creationDraft?.ipfsHash || 'Available after creation is created'}
+                </Box>
               </Grid>
             </Grid>
 
@@ -132,22 +98,22 @@ export default function StepThree({
           </Grid>
 
           {qrcodeBase64 && (
-          <Grid item md={5} xs={12} padding={4} display="flex" flexDirection="column" gap="8px" alignItems="center">
-            <Typography variant="h4" className="heading h4">Current QR Code</Typography>
-            <div className="create-collection-qrcode">
-              <img alt="qr" src={qrcodeBase64} />
-            </div>
-          </Grid>
+            <Grid item md={5} xs={12} padding={4} display="flex" flexDirection="column" gap="8px" alignItems="center">
+              <Typography variant="h4" className="heading h4">Current QR Code</Typography>
+              <div className="create-collection-qrcode">
+                <img alt="qr" src={qrcodeBase64} />
+              </div>
+            </Grid>
           )}
 
           <Grid xs={12} md={3} lg={2} marginTop={{ xs: '12px', md: '18px' }} display="flex" flexDirection="row" alignItems="center" />
           <Grid xs={12} md={9} lg={10} marginTop={{ xs: '12px', md: '18px' }}>
             {(status.error || status.success)
-            && (
-            <Box width="100%" className={`${status.success ? 'bg-green' : 'bg-red'} color-white`} padding="16px" borderRadius="12px" fontSize="16px">
-              {status.success ? (qrcodeBase64 ? 'Success! The creation was published. Redirecting...' : 'Success! A new creation was made. Redirecting...') : status.error}
-            </Box>
-            )}
+              && (
+                <Box width="100%" className={`${status.success ? 'bg-green' : 'bg-red'} color-white`} padding="16px" borderRadius="12px" fontSize="16px">
+                  {status.success ? (qrcodeBase64 ? 'Success! The creation was published. Redirecting...' : 'Success! A new creation was made. Redirecting...') : status.error}
+                </Box>
+              )}
           </Grid>
 
         </Grid>
@@ -158,10 +124,10 @@ export default function StepThree({
               <>
                 <Button className="backCollectionButton" onClick={onBack}>Back</Button>
                 {!qrcodeBase64 && (
-                <Button className="saveDraftButton" style={{ marginLeft: 'auto', marginRight: '12px' }} onClick={() => onComplete({ is_draft: true })}>
-                  {!loading ? 'Save Draft'
-                    : <div className="loader" />}
-                </Button>
+                  <Button className="saveDraftButton" style={{ marginLeft: 'auto', marginRight: '12px' }} onClick={() => onComplete({ is_draft: true })}>
+                    {!loading ? 'Save Draft'
+                      : <div className="loader" />}
+                  </Button>
                 )}
                 <Button disabled={loading} type="submit" className="nextCollectionButton" onClick={onComplete}>
                   {!loading ? (qrcodeBase64 ? 'Publish' : 'Finish')
