6a370db202873af81a11d739f3ca61b49c82ebff
huzaifa-99 <huzaifarasheed900@gmail.com>
2023-02-14 03:03:22 +0500

    make creation in multi step draft window

--- app/web-frontend/src/pages/Creations/Update/index.jsx
+++ app/web-frontend/src/pages/Creations/Update/index.jsx
@@ -1,22 +1,26 @@
 import {
   useNavigate,
   useParams,
+  useLocation,
 } from 'react-router-dom';
 import CreationForm from '../common/form';
 
 function UpdateCreation() {
   const { id } = useParams();
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const step = Number.parseInt(query.get('step'), 10);
   const navigate = useNavigate();
 
   const handleCreationFetch = (creation) => {
     if (creation && creation.original && !creation?.original?.is_draft) {
-      // redirect to 404 page if the creation is published
-      navigate('/404');
+      // redirect to creation details page if the creation is published
+      navigate(`/creations/${id}`);
     }
   };
 
   return (
-    <CreationForm id={id} onCreationFetch={handleCreationFetch} />
+    <CreationForm id={id} activeStep={step} onCreationFetch={handleCreationFetch} />
   );
 }
 
