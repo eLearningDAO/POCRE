edc25dbb7a20144849b53c9a73871407e5da02e1
huzaifa-99 <huzaifarasheed900@gmail.com>
2023-02-16 21:24:49 +0500

    update creation drafts in frontend

--- app/web-frontend/src/pages/Litigations/common/form/index.jsx
+++ app/web-frontend/src/pages/Litigations/common/form/index.jsx
@@ -1,9 +1,14 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Button } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './index.css';
+import Loader from 'components/uicore/Loader';
 import StepOne from './Steps/One';
 import useLitigationForm from './useLitigationForm';
 
-function LitigationForm() {
+function LitigationForm({ id = null, onLitigationFetch = () => {} }) {
+  const navigate = useNavigate();
+
   const {
     authorSuggestions,
     handleAuthorInputChange,
@@ -13,7 +18,34 @@ function LitigationForm() {
     newLitigationStatus,
     isCreatingLitigation,
     getMaterialDetail,
-  } = useLitigationForm();
+    getLitigationDetails,
+    isFetchingLitigation,
+    fetchLitigationStatus,
+    litigation,
+    deleteLitigationStatus,
+    isDeletingLitigation,
+    deleteLitigation,
+    resetDeletionErrors,
+  } = useLitigationForm({ onLitigationFetch });
+
+  useEffect(() => {
+    if (id) getLitigationDetails(id);
+  }, [id]);
+
+  useEffect(() => {
+    if (deleteLitigationStatus.success) navigate('/litigations');
+  }, [deleteLitigationStatus.success]);
+
+  if (id && isFetchingLitigation) return <Loader />;
+
+  if (id && fetchLitigationStatus.error) {
+    return (
+      <div className="creation-not-found">
+        <h4 className="heading h4">{fetchLitigationStatus.error}</h4>
+        <Button className="btn btn-primary" onClick={() => navigate(-1)}>Go Back</Button>
+      </div>
+    );
+  }
 
   return (
     <Grid container spacing={2}>
@@ -25,6 +57,11 @@ function LitigationForm() {
 
       <StepOne
         onComplete={async (values) => await makeNewLitigation({ ...values })}
+        initialValues={{
+          title: litigation?.litigation_title,
+          description: litigation?.litigation_description,
+          creation: litigation?.creation,
+        }}
         authorSuggestions={authorSuggestions}
         onAuthorInputChange={handleAuthorInputChange}
         creationSuggestions={creationSuggestions}
@@ -32,6 +69,11 @@ function LitigationForm() {
         getMaterialDetail={getMaterialDetail}
         status={newLitigationStatus}
         creatingLitigation={isCreatingLitigation}
+        handleLitigationDraftDelete={deleteLitigation}
+        isDeletingDraft={isDeletingLitigation}
+        deletionError={deleteLitigationStatus.error}
+        resetDeletionErrors={resetDeletionErrors}
+        isExistingDraft={litigation?.is_draft}
       />
     </Grid>
   );
--- app/web-frontend/src/pages/Litigations/common/form/useLitigationForm.jsx
+++ app/web-frontend/src/pages/Litigations/common/form/useLitigationForm.jsx
@@ -1,7 +1,8 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient, useQuery } from '@tanstack/react-query';
 import { Litigation, Material } from 'api/requests';
 import useSuggestions from 'hooks/useSuggestions';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import authUser from 'utils/helpers/authUser';
 import { CHARGES, TRANSACTION_PURPOSES } from 'config';
 import { transactADAToPOCRE } from 'utils/helpers/wallet';
@@ -9,9 +10,11 @@ import { transactADAToPOCRE } from 'utils/helpers/wallet';
 // get auth user
 const user = authUser.getUser();
 
-const useLitigationForm = () => {
+const useLitigationForm = ({ onLitigationFetch }) => {
+  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const [newLitigation, setNewLitigation] = useState(null);
+  const [litigationId, setLitigationId] = useState(null);
 
   const {
     suggestions: authorSuggestions,
@@ -30,6 +33,49 @@ const useLitigationForm = () => {
     search: 'creations',
   });
 
+  // get litigation details
+  const {
+    data: litigation,
+    isError: isFetchError,
+    isSuccess: isFetchSuccess,
+    isLoading: isFetchingLitigation,
+  } = useQuery({
+    queryKey: [`litigations-${litigationId}`],
+    queryFn: async () => {
+      // get litigation
+      const toPopulate = ['creation_id', 'creation_id.author_id', 'creation_id.materials.author_id', 'material_id', 'assumed_author'];
+      // eslint-disable-next-line sonarjs/no-empty-collection
+      return await Litigation.getById(litigationId, toPopulate.map((x) => `populate=${x}`).join('&'));
+    },
+    enabled: !!litigationId,
+  });
+
+  // delete creation
+  const {
+    mutate: deleteLitigation,
+    isError: isDeleteError,
+    isSuccess: isDeleteSuccess,
+    isLoading: isDeletingLitigation,
+    reset: resetDeletionErrors,
+  } = useMutation({
+    mutationFn: async () => {
+      await Litigation.delete(litigationId);
+
+      // update queries
+      queryClient.cancelQueries({ queryKey: ['litigations'] });
+      queryClient.setQueryData(['litigations'], (data) => {
+        if (data && data?.results) {
+          const temporaryLitigations = data?.results?.filter(
+            (x) => x?.litigation_id !== litigationId,
+          );
+
+          return { ...data, results: [...temporaryLitigations] };
+        }
+        return data;
+      });
+    },
+  });
+
   // create new litigation
   const {
     mutate: makeNewLitigation,
@@ -38,34 +84,46 @@ const useLitigationForm = () => {
     isLoading: isCreatingLitigation,
   } = useMutation({
     mutationFn: async (litigationBody = {}) => {
-      // make transaction
-      const txHash = await transactADAToPOCRE({
-        amountADA: CHARGES.LITIGATION.START,
-        purposeDesc: TRANSACTION_PURPOSES.LITIGATION.START,
-        walletName: authUser.getUser()?.selectedWallet,
-        metaData: {
-          claimed_entity: litigationBody.material ? 'MATERIAL' : 'CREATION',
-          creation_id: litigationBody.creation,
-          material_id: litigationBody.material,
-        },
-      });
-
-      if (!txHash) throw new Error('Failed to make transaction');
-
       // make a new litigation
-      const response = await Litigation.create({
+      const payload = {
         litigation_title: litigationBody.title.trim(),
         litigation_description: litigationBody?.description?.trim(),
         creation_id: litigationBody.creation,
         material_id: litigationBody.material,
-      });
-
+        is_draft: litigationBody.is_draft,
+      };
+      const response = litigation?.litigation_id
+        ? await Litigation.update(litigation?.litigation_id, payload)
+        : await Litigation.create(payload);
       setNewLitigation(response);
+
+      // make transaction
+      if (!litigationBody.is_draft) {
+        const txHash = await transactADAToPOCRE({
+          amountADA: CHARGES.LITIGATION.START,
+          purposeDesc: TRANSACTION_PURPOSES.LITIGATION.START,
+          walletName: authUser.getUser()?.selectedWallet,
+          metaData: {
+            claimed_entity: litigationBody.material ? 'MATERIAL' : 'CREATION',
+            creation_id: litigationBody.creation,
+            material_id: litigationBody.material,
+          },
+        });
+
+        if (!txHash) throw new Error('Failed to make transaction');
+      }
 
       // update queries
       queryClient.invalidateQueries({ queryKey: ['litigations'] });
+
+      setTimeout(() => navigate('/litigations'), 2000);
     },
   });
+
+  // trigger the callback when a litigation is fetched
+  useEffect(() => {
+    if (litigation) onLitigationFetch(litigation);
+  }, [litigation]);
 
   return {
     isCreatingLitigation,
@@ -82,6 +140,20 @@ const useLitigationForm = () => {
     handleCreationInputChange,
     handleAuthorInputChange,
     getMaterialDetail: Material.getById,
+    getLitigationDetails: (id) => setLitigationId(id),
+    fetchLitigationStatus: {
+      success: isFetchSuccess,
+      error: isFetchError ? 'Litigation Not Found' : null,
+    },
+    isFetchingLitigation,
+    litigation,
+    deleteLitigationStatus: {
+      success: isDeleteSuccess,
+      error: isDeleteError ? 'Failed to delete litigation' : null,
+    },
+    isDeletingLitigation,
+    deleteLitigation,
+    resetDeletionErrors,
   };
 };
 
